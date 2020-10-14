d3612c6a770e9149a025c199e562ee9660553462
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-14 10:19:38 -0400

    wrote first fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const baseUrl = "http://localhost:3000/dogs"
 
+    const getDogs = () => {
+        fetch(baseUrl)
+        .then (response => response.json())
+        .then (dogs => console.log(dogs))
+    }
+
+    getDogs()
 })
+
+
+
+//render list of dogs on table 
+//<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
+// table body: id="table-body"
+
+
+//clicking on edit button should add dog to top form 
+
+//submiting form sends patch request to updsate dog
+
+//after submit, table should have all updated info 
