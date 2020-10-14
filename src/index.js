a4d74421c127b3b5ecc3be4c8ae9caa4225baa48
D1yr <alexander.m.sahli@gmail.com>
2020-10-14 11:12:30 -0400

    got table to append so thats a plus

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 
-const dTable = document.getElementById("table-body")
 
-console.log(dTable)
 
-fetch("http://localhost:3000/dogs/")
-    .then(resp => resp.json())
-    .then(json => addDogsToDom(json))
 
-function addDogsToDom(dogs){
-    dogs
-}
 
 document.addEventListener('DOMContentLoaded', () => {
 
 
+    fetch("http://localhost:3000/dogs/")
+    .then(resp => resp.json())
+    .then(json => addDogsToDom(json))
+
+function addDogsToDom(dogs) {
+    dogs.forEach(function(dog){
+        addDogToTable(dog)
+    })
+}
+
+function addDogToTable(dog) {
+    const dTable = document.getElementById("table-body")
+	const newDogTr = document.createElement("tr");
+	newDogTr.innerHTML = `<tr>
+		<td>${dog.name}</td> 
+		<td>${dog.breed}</td> 
+		<td>${dog.sex}</td> 
+		<td><button data-id=${dog.id}>Edit</button></td>
+	</tr>`
+	dTable.append(newDogTr);
+
+
+}
 })
