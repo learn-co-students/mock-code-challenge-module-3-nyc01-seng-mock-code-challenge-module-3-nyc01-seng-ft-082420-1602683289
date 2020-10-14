6b4cc7279aee8398f5e61552f821dc75a6012ae6
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-14 10:34:56 -0400

    added click listeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchDogs()
+    editDog()
+    submitHander()
 })
+
 
 function fetchDogs() {
     fetch('http://localhost:3000/dogs')
@@ -20,8 +23,43 @@ function renderDog(dog) {
     <td>${dog.name}</td>
     <td>${dog.breed}</td>
     <td>${dog.sex}</td>
-    <td><button>Edit</button></td></tr>
+    <td><button class="edit" data-id=${dog.id}>Edit</button></td></tr>
     `
     const dogTable = document.querySelector("#table-body")
     dogTable.append(dogRow)
+}
+
+function editDog() {
+    const form = document.querySelector("#dog-form")
+    document.addEventListener("click", e=> {
+        if (e.target.matches(".edit")) {
+            //form.name.value = e.target.nextSibling.textContent
+            console.log(e.target.dataset.id)
+            const hiddenId = document.createElement("input")
+            hiddenId.setAttribute("type", "hidden");
+            hiddenId.setAttribute("name", "id");
+            hiddenId..setAttribute("value", `${e.target.data.set.id}`);
+        }
+    })
+}
+
+function submitHander() {
+    const form = document.querySelector("#dog-form")
+    form.addEventListener("submit", e => {
+        e.preventDefault()
+        const body = {
+            name: form.name.value,
+            breed: form.breed.value,
+            sex: form.sex.value
+        }
+        const options = {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+                accept: "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        fetch
+    })
 }
