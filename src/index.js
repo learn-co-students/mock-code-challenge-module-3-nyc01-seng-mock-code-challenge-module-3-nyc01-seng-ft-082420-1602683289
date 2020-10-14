80764db5e5c69bcb39f0e50955746fd2552c7bf6
mahdihawary <Mahdihawary@gmail.com>
2020-10-14 11:12:56 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const URL = 'http://localhost:3000/dogs'
+    const URL = 'http://localhost:3000/dogs/'
     const form = document.querySelector("#dog-form")
 
 
     renderDogs = (dogs) => {
+        
         const table = document.querySelector("#table-body")
-
+        
         for (const dog of dogs) {
             const newDogEl = document.createElement("tr")
             newDogEl.innerHTML = `<td class = "name">${dog.name}</td> <td class = "breed">${dog.breed}</td> <td class = "sex">${dog.sex}</td> <td><button class="edit">Edit</button></td>
@@ -30,26 +31,40 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     form.addEventListener('submit', function (e) {
-        console.log(e.target)
-        newDog ={}
-        options = {method: "PATCH",
+        
+        e.preventDefault()
+        newDog = {
+            name: `${form.name.value}`,
+            breed: `${form.breed.value}`,
+            sex: `${form.sex.value}`
+        }
+        options = {
+            method: "PATCH",
             headers: {
                 "content-type": "application/json",
                 "accept": "application/json"
             },
-            body:JSON.stringify()
+            body: JSON.stringify(newDog)
         }
-        // fetch(URL + , options)
+        fetch(URL + `${form.dataset.dogId}`, options)
+            .then(res => res.json())
+            .then(dogs => {
+                console.log(dogs)
+            })
+            getDogs()
+            e.target.reset()
+            
     })
 
 
 
+    getDogs = () => {
+        fetch(URL)
+            .then(res => res.json())
+            .then(dogs => {
+                renderDogs(dogs)
+            })
+    }
 
-    fetch(URL)
-        .then(res => res.json())
-        .then(dogs => {
-            renderDogs(dogs)
-        })
-
-
+    getDogs()
 })
