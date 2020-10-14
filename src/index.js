0b2367e06178ffb4b2f57994ec5a3e3ab24bfe1a
jdhofmann <justindhofmann@gmail.com>
2020-10-14 11:28:57 -0400

    added unnecessary click function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
-/*
-√ - On page load, render a list of already registered dogs in the table. You can fetch these dogs from http://localhost:3000/dogs.
-
-√ - The dog should be put on the table as a table row. The HTML might look something like this `<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>`
-
-√ - Make a dog editable. Clicking on the edit button next to a dog should populate the top form with that dog's current information.
-
-√ √ - On submit of the form, a PATCH request should be made to http://localhost:3000/dogs/:id to update the dog information (including name, breed and sex attributes).
-
-- Once the form is submitted, the table should reflect the updated dog information. There are many ways to do this. You could search for the table fields you need to edit and update each of them in turn, but we suggest making a new get request for all dogs and rerendering all of them in the table. Make sure this GET happens after the PATCH so you can get the most up-to-date dog information. */
 
 const baseUrl = `http://localhost:3000/dogs/`;
 
 document.addEventListener('DOMContentLoaded', () => {
   
-  // fetch all dogs from http://localhost:3000/dogs
-
   const getDogs = () => {
-    // console.log('getting dogs')
     return fetch(baseUrl)
     .then(response => response.json())
     .then(data => renderDogsTable(data))
   }
 
-  // render all dogs from ^^ as table row 
-    // add id to edit button
-
   const renderDogsTable = (data) => {
+    // re-render only works with this next line
     document.querySelector('tbody').innerHTML = '';
     for(const dog of data){
       renderDog(dog)
@@ -33,18 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const renderDog = (dog) => {
-    // console.log(dog)
     let dogRow = document.createElement('tr');
     dogRow.innerHTML = `
     <td>Dog ${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button data-id="${dog.id}" data-name="${dog.name}" data-breed="${dog.breed}" data-sex="${dog.sex}">Edit</button></td>
     `
     document.querySelector('tbody').append(dogRow);
-    // console.log(dogRow)
   }
-
-  // edit button click
-    // fill out form with dog info
-    // 
 
   const clickHandler = () => {
     document.addEventListener('click', e => {
@@ -53,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let name = e.target.dataset.name
         let breed = e.target.dataset.breed
         let sex = e.target.dataset.sex
-
+        
         const dogForm = document.querySelector('#dog-form');
         dogForm.dataset.dogId = id;
         let formName = dogForm.querySelector('input[name="name"]');
@@ -62,6 +41,15 @@ document.addEventListener('DOMContentLoaded', () => {
         formName.value = name;
         formBreed.value = breed;
         formSex.value = sex;
+
+        /// extra style thing:
+        let tBody = document.querySelector('tbody')
+        let allRows = tBody.children
+        for(const row of allRows){
+          row.style.background = "none"
+        }
+        let row = e.target.parentElement.parentElement
+        row.style.background = "#ffc2c2"
       }
     })
   }
@@ -83,21 +71,13 @@ document.addEventListener('DOMContentLoaded', () => {
           sex: dogForm.sex.value,
         })
       }
-      // console.log(options)
       fetch(baseUrl + id, options)
       .then(response => response.json())
-      .then(function(dog){
-        // console.log(dog)
-        getDogs()
-      })
+      .then(getDogs)
 
       dogForm.reset();
     })
   }
-  // form submit
-    // patch request to http://localhost:3000/dogs/:id 
-    // update dog's info - suggested calling render dogs table again
-
 
   submitHandler();
   clickHandler();
