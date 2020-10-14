55050d2a18289d07480abe589c1f4ff718aa7da2
gerardmi24 <gerardmistretta@yahoo.com>
2020-10-14 10:34:43 -0400

    Progress Report

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+
+
+//fetchDogs
+
 })
+
+//const fetchDogs = () => {
+fetch('http://localhost:3000/dogs')
+.then(resp => resp.json())
+.then(dogs => renderDogs(dogs));
+
+//}
+
+const renderDogs = (dogs) => {
+for (const dog of dogs){
+    addDogToDom(dog)
+}
+
+}
+
+const addDogToDom = (dog) => {
+    let tableBody = document.getElementById('table-body')
+
+
+
+}
+
+
+
+
+// document.addEventListener("click", function(e));
+
+// function e(){
+//    body of function
+//}
