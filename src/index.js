fa1eaf03ac55bbc06661307d6ba01a73a3937266
github username <github email address>
2020-10-14 10:34:48 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+const fetchUrl = " http://localhost:3000/dogs"
+const tableBody = document.querySelector("tbody#table-body")
+console.log(tableBody)
+
+const getDogs = () => {
+    fetch(fetchUrl)
+    .then(response => response.json())
+    .then(dog => {
+        renderDogs(dog)
+    })
+}
+
+const renderDogs = (dog) => {
+    for (const dogObj of dog) {
+        // console.log(dogObj)
+        renderDog(dogObj)
+    }
+}
+
+const renderDog = (dogObj) => {
+    const trTag = document.createElement("tr")
+    // trTag.dataset.id = `${dogObj.id}`
+    // trTag.innerText = `${dogObj.name}`
+    // trTag.innerText = `${dogObj.breed}`
+    // trTag.innerText = `${dogObj.sex}`
+    
+
+    
+    console.log(dogObj)
+}
+ 
+
+
+getDogs()
 })
