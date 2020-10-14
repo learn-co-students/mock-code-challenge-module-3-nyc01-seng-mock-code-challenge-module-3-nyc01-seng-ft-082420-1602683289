bb7e6e8a577c77dd5defd048dfd6898c16a89a54
Sean LaFlam <sean.laflam@gmail.com>
2020-10-14 07:51:57 -0700

    Get and Patch working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const dogUrl = 'http://localhost:3000/dogs/'
 
+  const getDogs = () => {
+    const tableBody = document.getElementById('table-body')
+    fetch(dogUrl)
+    .then(response => response.json())
+    .then(dogData => {
+      for (dog of dogData){
+        const tableRow = document.createElement('tr')
+        tableRow.innerHTML = `
+          <td>${dog.name}</td>
+          <td>${dog.breed}</td>
+          <td>${dog.sex}</td>
+          <td><button data-dog-id= "${dog.id}">Edit</button></td>
+        `
+        tableBody.append(tableRow)
+      }
+    })
+  }
+
+  const editDog = () => {
+    document.addEventListener('click', e => {
+      if (e.target.matches('[data-dog-id]')) {
+        const dogId = e.target.dataset.dogId
+        fetch(dogUrl + dogId)
+        .then(response => response.json())
+        .then(dogData => {
+          dogForm = document.getElementById('dog-form')
+          let inputs = dogForm.querySelectorAll("input")
+          inputs[0].value = dogData.name
+          inputs[1].value = dogData.breed
+          inputs[2].value = dogData.sex
+          inputs[3].id = dogData.id
+        })
+      }
+    })
+  }
+
+  const postDog = () => {
+    document.addEventListener('submit', e => {
+      e.preventDefault()
+      if (e.target.matches('#dog-form')){
+        const dogForm = e.target
+        let inputs = dogForm.querySelectorAll("input")
+        let dogId = inputs[3].id
+        options = {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+          body: JSON.stringify({
+            name: inputs[0].value,
+            breed: inputs[1].value,
+            sex: inputs[2].value,
+          })
+        }
+
+        fetch(dogUrl+dogId, options)
+        .then(response => response.json())
+        .then(newDog => {
+          console.log(newDog)
+        })
+      }
+    })
+  }
+
+  getDogs()
+  editDog()
+  postDog()
 })
