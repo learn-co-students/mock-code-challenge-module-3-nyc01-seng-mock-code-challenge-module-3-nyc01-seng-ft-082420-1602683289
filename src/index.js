6b91c84e673d421fe8b3dfc1bd78cfe76f1d0f0f
D1yr <alexander.m.sahli@gmail.com>
2020-10-14 10:34:46 -0400

    first commit alexS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+const BASE = "http://localhost:3000"
+const DOGS = `${BASE}/dogs/`
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const getDogs = () => {
+        fetch(DOGS)
+        .then(resp => resp.json())
+        .then(dogs => addDogs(dogs))
+    }
+    const addDogs = () => {
+        for(const dog of dogs){
+            addDogToDom(dog)
+        }
+    }
+    const addDogToDom = (dog) => {
+        let tbody = document.querySelector('tbody')
+        
+
+    }
+    
 
 })
