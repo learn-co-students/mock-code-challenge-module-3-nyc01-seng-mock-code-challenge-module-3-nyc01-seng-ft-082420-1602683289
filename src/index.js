5a78fc43e209346fe38c1f256b603f8f038a449a
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-14 11:41:40 -0400

    in the middle of writing fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const baseUrl = "http://localhost:3000/dogs"
     const dogListBody = document.querySelector('#table-body')
+    let dogId = 0
 
     const renderDogList = (dogs) => {
         for (const dog of dogs) {
@@ -27,13 +28,42 @@ document.addEventListener('DOMContentLoaded', () => {
     const clickHandler = () => {
         document.addEventListener('click', e => {
             if (e.target.matches('.edit-button')) {
-                let dogId = e.target.dataset.id
+                dogId = e.target.dataset.id
                 //console.log(dogId)
                 let dogRow = e.target.parentElement.parentElement
                 const dogForm = document.querySelector('#dog-form')
                 let dogName = dogRow.cells[0].innerHTML
-                console.log(dogName)
+                let dogBreed = dogRow.cells[1].innerHTML
+                let dogSex = dogRow.cells[2].innerHTML
+                dogForm.querySelector("#name").value = dogName
+                dogForm.querySelector("#breed").value = dogBreed
+                dogForm.querySelector("#sex").value = dogSex
+            } 
+        })
+    }
+
+    const submitHandler = () => {
+        document.addEventListener('submit', e => {
+            e.preventDefault()
+            const form = e.target
+            const name = form.name.value
+            const breed = form.breed.value
+            const sex = form.breed.value
+            const id = DogId
+            const newDog = { id: id, name: name, breed: breed, sex: sex }
+
+            const options = {
+                method: "PATCH",
+                headers: {
+                    "content-type": "appplication/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify(newDog)
             }
+
+
+
+            form.reset()
         })
     }
 
