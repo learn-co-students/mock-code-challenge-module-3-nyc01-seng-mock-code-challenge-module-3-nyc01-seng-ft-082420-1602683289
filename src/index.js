c682f88d993473879655a51c189d378399dccc6a
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-14 10:34:42 -0400

    made dog list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const baseUrl = "http://localhost:3000/dogs"
+    const dogListBody = document.querySelector('#table-body')
+
+    const renderDogList = (dogs) => {
+        for (const dog of dogs) {
+            renderDogRow(dog)
+
+
+        }
+
+    }
+
+    const renderDogRow = (dog) => {
+        let newDogRow = dogListBody.insertRow(-1)
+        newDogRow.innerHTML = `<tr><td>${dog.name}</td> 
+        <td>${dog.breed}</td> 
+        <td>${dog.sex}</td> 
+        <td><button class="edit-button" data-id="${dog.id}">Edit</button></td></tr>`
+
+    }
 
     const getDogs = () => {
         fetch(baseUrl)
         .then (response => response.json())
-        .then (dogs => console.log(dogs))
+        .then (dogs => renderDogList(dogs))
     }
 
     getDogs()
@@ -15,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
 //render list of dogs on table 
 //<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
 // table body: id="table-body"
+//let specific_tbody = document.getElementById(tbody_id);
+//let row = specific_tbody.insertRow(index)
 
 
 //clicking on edit button should add dog to top form 
