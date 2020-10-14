1b2a85c9c8008e75ce81232b1d96ba0bf675fc82
gabrielhicks <gabrielhicks@mail.com>
2020-10-14 10:52:09 -0400

    Next commit, finishing form;

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function renderDogs(dogs) {
 
 function renderDog(dog, tableBody) {
     let tableRow = document.createElement("tr")
+    tableRow.id = dog.name
     tableRow.innerHTML = `
     <td>${dog.name}</td><td>${dog.breed}</td><td>${dog.sex}</td><td><button id="${dog.id}">Edit Dog</button></td>
     `
@@ -28,12 +29,11 @@ function clickHandler() {
     document.addEventListener("click", e => {
         if(e.target.matches("button")) {
         const form = document.querySelector("#dog-form")
-        let inputName = form.name.value
-        let inputBreed = form.name.value
-        let inputSex = form.sex.value
         let dogId = e.target.id
         let dogData = e.target.parentElement.parentElement
-        console.dir(e.target.parentElement.parentElement)
+        form.name.value = dogData.children[0].innerText
+        form.breed.value = dogData.children[1].innerText
+        form.sex.value = dogData.children[2].innerText
         }
     })
 }
@@ -41,6 +41,28 @@ function clickHandler() {
 function submitHandler() {
     document.addEventListener("submit", e => {
         e.preventDefault()
-        console.log(e.target)
+        const form = document.querySelector("#dog-form")
+        let dogName = form.name.value
+        let dogElement = document.querySelector(`#${dogName}`)
+        let dogId = dogElement.children[3].firstChild.id
+        let dogBreed = form.breed.value
+        let dogSex = form.sex.value
+        // updateDog()
+        console.log(dogId)
+    })
+}
+
+function updateDog(id, name, breed, sex) {
+    const options = {
+        method: "PATCH",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({ "name": name, "breed": breed, "sex": sex})
+    }
+    fetch(DOGS_URL + id, options)
+    .then(response => response.json())
+    .then(dog => {
+        console.log(dog)
     })
 }
