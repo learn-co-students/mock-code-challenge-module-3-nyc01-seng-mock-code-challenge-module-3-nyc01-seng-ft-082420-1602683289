14457402fad905885b0e7b766571598c5ee75cf9
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-14 10:35:08 -0400

    fetched

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+    fetchDogs();
 })
+
+const dogsUrl = "http://localhost:3000/dogs/"
+
+const fetchDogs = () => {
+    fetch(dogsUrl)
+    .then(resp => resp.json())
+    .then(dogs => addDogs(dogs))
+}
+
+const addDogs = (dogs) => {
+    for(const dog of dogs){
+        addDogToDom(dog)
+    }
+}
+
+const addDogToDom = (dog) => {
+    let table = document.querySelector('tr')
+    
+}
+
