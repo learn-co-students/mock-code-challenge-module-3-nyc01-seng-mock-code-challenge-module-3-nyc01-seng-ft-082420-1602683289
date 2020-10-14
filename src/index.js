0962d5627b400141da7c90b0a356c7f5cc7573ed
aribakhan97 <akhan41@binghamton.edu>
2020-10-14 11:13:07 -0400

    3rd commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// first things first is DOM content loaded which is already provided
 document.addEventListener('DOMContentLoaded', () => {
 const baseUrl = 'http://localhost:3000/dogs/'
-
+// always add the backslash at the end for easier use later
 const getDogs = () => {
     return fetch(baseUrl)
     .then(response => response.json())
 }
+// making request to get all the dog infomation 
 
 const showOneDog = (dog) => {
     const oneDog = document.querySelector('#table-body')
@@ -16,23 +18,56 @@ const showOneDog = (dog) => {
     const displaySex = document.createElement('td')
     displaySex.innerHTML = dog.sex
 
+    // getting info from inspect and using the HTML provided to create Element
+    // refer to readMe to get td and tr
+
     const displayButton = document.createElement('td')
     displayButton.innerHTML = `
     <button dataset-id= ${dog.id} type="button">Edit</button>
     `
     displayButton.addEventListener('click', e => {
-        const 
+        handleEdit(e, dog) 
     })
+    // getting button to actually respond to a click and change it to say edit
+    // this needs to take in two arguments the event and dog  
 
+    displayRow.append(displayName, displayBreed, displaySex,displayButton)
+    // displayRow.append(displayBreed)
+    // displayRow.append(displaySex)
+    // displayRow.append(displayButton)
 
-    displayRow.append(displayName)
-    displayRow.append(displayBreed)
-    displayRow.append(displaySex)
-    displayRow.append(displayButton)
+    // once we display all these above we need to append below 
     oneDog.append(displayRow)
 }
+// need to append to actually display all this info 
+const handleEdit = (e, dog) => {
+    const dogForm = document.querySelector('#dog-form')
+    populateForm(dog, dogForm)
+    dogForm.addEventListener('click', e => {
+        const newDog = {
+            name:dogForm.name.value,
+            breed:dogForm.breed.value,
+            sex:dogForm.sex.value         
+        }
 
+        const options = {
+            method: 'PATCH', 
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newDog),
+        }
+        fetch(baseUrl + dog.id, options )
+        .then(response => response.json())
+    })
+    // looked up syntax to make sure it was correct 
+}
 
+const populateForm = (dog, dogForm) => {
+    dogForm.name.value = dog.name
+    dogForm.breed.value = dog.breed
+    dogForm.sex.value = dog.sex
+}
 
 const showDogs = () => {
     getDogs()
@@ -41,7 +76,6 @@ const showDogs = () => {
         for(const dog of response){
         showOneDog(dog)
         }
-
     })
     
 }
