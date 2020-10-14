7a66e37f92e3e4ddbf5790f01beb882bb540eafa
skanagui <s.kanagui@gmail.com>
2020-10-14 10:34:45 -0400

    rendering dogs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const BASE_URL = "http://localhost:3000/dogs/"
+    
+    //get existing dogs on show page 
+    const getDogs = () => {
+        return fetch(BASE_URL)
+        .then(resp => resp.json())
+        .then(renderDogs)
+    }
+
+    const renderDogs = dogs => {
+        dogs.forEach(dogObj => {
+            renderDog(dogObj)
+        })
+
+    }
+
+    const renderDog = (dogObj) => {
+        
+    }
+
+
+    
+
 })
