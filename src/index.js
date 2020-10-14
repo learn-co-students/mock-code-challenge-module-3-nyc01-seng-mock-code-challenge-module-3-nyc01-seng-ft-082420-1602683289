2c3551ce5818792850af738060a7da187ae500da
Mamadoubarry10 <Mamadou.barry1105@gmail.com>
2020-10-14 10:34:54 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+const url = "http://localhost:3000/dogs"
 
+const renderDogs = (dogs) =>{
+    for(const dog of dogs){
+        console.log(dog.name)
+        renderDog(dog)
+    }
+}
+
+const renderDog = (dog) =>{
+    const thread = document.querySelector(".blue")
+    console.log(thread)
+    const table = `<tr><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit</button></td></tr>`
+    thread.insertAdjacentHTML('beforeend', table)
+}
+
+const getDogs = () =>{
+    fetch(url)
+    .then(res => res.json())
+    .then(dogs => renderDogs(dogs))
+}
+
+getDogs()
 })
